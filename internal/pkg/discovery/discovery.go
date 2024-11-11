8ed897c07b95a9da7985d68964e246f9444c09b3
Sebastian Widmer <sebastian.widmer@vshn.net>
2024-11-11 17:33:09 +0100

    Tests, CI, envtest setup

---
+++ internal/pkg/discovery/discovery.go
@@ -0,0 +1,107 @@
+package discovery
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"slices"
+	"strings"
+
+	"go.uber.org/multierr"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/rest"
+)
+
+type DiscoveryOptions struct {
+	BatchSize int64
+	LogWriter io.Writer
+}
+
+// GetBatchSize returns the set batch size for listing objects or the default.
+func (opts DiscoveryOptions) GetBatchSize() int64 {
+	if opts.BatchSize == 0 {
+		return 500
+	}
+	return opts.BatchSize
+}
+
+// GetLogWriter returns the set batch size for listing objects or io.Discard as default.
+func (opts DiscoveryOptions) GetLogWriter() io.Writer {
+	if opts.LogWriter == nil {
+		return io.Discard
+	}
+	return opts.LogWriter
+}
+
+// DiscoverObjects discovers all objects in the cluster and calls the provided callback for each list of objects.
+// The callback can be called multiple times with the same
+func DiscoverObjects(ctx context.Context, conf *rest.Config, cb func(*unstructured.UnstructuredList) error, opts DiscoveryOptions) error {
+	batchSize := opts.GetBatchSize()
+	logWriter := opts.GetLogWriter()
+
+	dc, err := discovery.NewDiscoveryClientForConfig(conf)
+	if err != nil {
+		return fmt.Errorf("failed to create discovery client: %w", err)
+	}
+	dynClient, err := dynamic.NewForConfig(conf)
+	if err != nil {
+		return fmt.Errorf("failed to create dynamic client: %w", err)
+	}
+
+	sprl, err := dc.ServerPreferredResources()
+	if err != nil {
+		return fmt.Errorf("failed to get server preferred resources: %w", err)
+	}
+
+	fmt.Fprintln(logWriter, "Discovered resources:")
+	for _, re := range sprl {
+		fmt.Fprintln(logWriter, re.GroupVersion)
+		for _, r := range re.APIResources {
+			fmt.Fprintln(logWriter, "  ", r.Kind)
+		}
+	}
+
+	var errors []error
+	for _, re := range sprl {
+		for _, r := range re.APIResources {
+			res := groupVersionFromString(re.GroupVersion).WithResource(r.Name)
+			if !slices.Contains(r.Verbs, "list") {
+				fmt.Fprintf(logWriter, "skipping %s: no list verb\n", res)
+				continue
+			}
+
+			continueKey := ""
+			for {
+				l, err := dynClient.Resource(res).List(context.TODO(), metav1.ListOptions{
+					Limit:    batchSize,
+					Continue: continueKey,
+				})
+				if err != nil {
+					errors = append(errors, fmt.Errorf("failed to list %s: %w", res, err))
+					break
+				}
+				if err := cb(l); err != nil {
+					errors = append(errors, fmt.Errorf("failed to dump %s: %w", res, err))
+				}
+				if l.GetContinue() == "" {
+					break
+				}
+				continueKey = l.GetContinue()
+			}
+		}
+	}
+
+	return multierr.Combine(errors...)
+}
+
+func groupVersionFromString(s string) schema.GroupVersion {
+	parts := strings.Split(s, "/")
+	if len(parts) == 1 {
+		return schema.GroupVersion{Version: parts[0]}
+	}
+	return schema.GroupVersion{Group: parts[0], Version: parts[1]}
+}
--- main.go
+++ main.go
@@ -1,28 +1,17 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"os"
-	"path/filepath"
-	"slices"
-	"strings"
 
-	"go.uber.org/multierr"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/discovery"
-	"k8s.io/client-go/dynamic"
 	"rsc.io/getopt"
 	ctrl "sigs.k8s.io/controller-runtime"
-)
 
-type dumperFunc func(*unstructured.UnstructuredList) error
+	"github.com/bastjan/k8s-object-dumper/internal/pkg/discovery"
+	"github.com/bastjan/k8s-object-dumper/internal/pkg/dumper"
+)
 
 func main() {
 	var dir string
@@ -33,13 +22,13 @@ func main() {
 
 	getopt.Parse()
 
-	df := dumpJSONToWriter(os.Stdout)
+	df := dumper.DumpToWriter(os.Stdout)
 	if dir != "" {
 		if err := os.MkdirAll(dir, 0755); err != nil {
 			fmt.Fprintf(os.Stderr, "failed to create directory %s: %v\n", dir, err)
 			os.Exit(1)
 		}
-		d, err := newDirDumper(dir)
+		d, err := dumper.NewDirDumper(dir)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "failed to create directory dumper: %v\n", err)
 			os.Exit(1)
@@ -48,184 +37,16 @@ func main() {
 		df = d.Dump
 	}
 
-	if err := dumpObjects(df, batchSize, os.Stderr); err != nil {
+	conf, err := ctrl.GetConfig()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to get Kubernetes config: %v", err)
+	}
+
+	if err := discovery.DiscoverObjects(context.Background(), conf, df, discovery.DiscoveryOptions{
+		BatchSize: batchSize,
+		LogWriter: os.Stderr,
+	}); err != nil {
 		fmt.Fprintf(os.Stderr, "failed to dump some or all objects: %+v\n", err)
 		os.Exit(1)
 	}
-}
-
-func dumpObjects(dumper dumperFunc, batchSize int64, logWriter io.Writer) error {
-	conf, err := ctrl.GetConfig()
-	if err != nil {
-		return fmt.Errorf("failed to get Kubernetes config: %w", err)
-	}
-	dc, err := discovery.NewDiscoveryClientForConfig(conf)
-	if err != nil {
-		return fmt.Errorf("failed to create discovery client: %w", err)
-	}
-	dynClient, err := dynamic.NewForConfig(conf)
-	if err != nil {
-		return fmt.Errorf("failed to create dynamic client: %w", err)
-	}
-
-	sprl, err := dc.ServerPreferredResources()
-	if err != nil {
-		return fmt.Errorf("failed to get server preferred resources: %w", err)
-	}
-
-	fmt.Fprintln(logWriter, "Discovered resources:")
-	for _, re := range sprl {
-		fmt.Fprintln(logWriter, re.GroupVersion)
-		for _, r := range re.APIResources {
-			fmt.Fprintln(logWriter, "  ", r.Kind)
-		}
-	}
-
-	var errors []error
-	for _, re := range sprl {
-		for _, r := range re.APIResources {
-			res := groupVersionFromString(re.GroupVersion).WithResource(r.Name)
-			if !slices.Contains(r.Verbs, "list") {
-				fmt.Fprintf(logWriter, "skipping %s: no list verb\n", res)
-				continue
-			}
-
-			continueKey := ""
-			for {
-				l, err := dynClient.Resource(res).List(context.TODO(), metav1.ListOptions{
-					Limit:    batchSize,
-					Continue: continueKey,
-				})
-				if err != nil {
-					errors = append(errors, fmt.Errorf("failed to list %s: %w", res, err))
-					break
-				}
-				if err := dumper(l); err != nil {
-					errors = append(errors, fmt.Errorf("failed to dump %s: %w", res, err))
-				}
-				if l.GetContinue() == "" {
-					break
-				}
-				continueKey = l.GetContinue()
-			}
-		}
-	}
-
-	return multierr.Combine(errors...)
-}
-
-func dumpJSONToWriter(w io.Writer) dumperFunc {
-	return func(l *unstructured.UnstructuredList) error {
-		return json.NewEncoder(w).Encode(l)
-	}
-}
-
-// dirDumper writes objects to a directory.
-// Must be initialized with newDirDumper.
-// Must be closed after use.
-type dirDumper struct {
-	dir string
-
-	openFiles map[string]*os.File
-	sharedBuf *bytes.Buffer
-}
-
-// newDirDumper creates a new dirDumper that writes objects to the given directory.
-// The directory will be created if it does not exist.
-// If the directory cannot be created, an error is returned.
-func newDirDumper(dir string) (*dirDumper, error) {
-	if err := os.MkdirAll(dir, 0755); err != nil {
-		return nil, fmt.Errorf("failed to create directory %q: %w", dir, err)
-	}
-	return &dirDumper{
-		dir:       dir,
-		openFiles: make(map[string]*os.File),
-		sharedBuf: new(bytes.Buffer),
-	}, nil
-}
-
-// Close closes the dirDumper and all open files.
-// The dirDumper cannot be used after it is closed.
-func (d *dirDumper) Close() error {
-	var errs []error
-	for _, f := range d.openFiles {
-		if err := f.Close(); err != nil {
-			errs = append(errs, err)
-		}
-	}
-	return multierr.Combine(errs...)
-}
-
-// Dump writes the objects in the list to the directory.
-// The objects are written to the directory in two ways:
-// - All objects are written to a file named objects-<kind>.json
-// - Objects with a namespace are written to a directory named split/<namespace> with two files:
-//   - __all__.json contains all objects in the namespace
-//   - <kind>.json contains all objects of the kind in the namespace
-//
-// If an object cannot be written, an error is returned.
-// This method is not safe for concurrent use.
-func (d *dirDumper) Dump(l *unstructured.UnstructuredList) error {
-	buf := d.sharedBuf
-	var errs []error
-	for _, o := range l.Items {
-		buf.Reset()
-		if err := json.NewEncoder(buf).Encode(o); err != nil {
-			errs = append(errs, fmt.Errorf("failed to encode object: %w", err))
-			continue
-		}
-		p := buf.Bytes()
-
-		if err := d.writeToFile(fmt.Sprintf("%s/objects-%s.json", d.dir, o.GetKind()), p); err != nil {
-			errs = append(errs, err)
-		}
-
-		if o.GetNamespace() == "" {
-			continue
-		}
-
-		if err := d.writeToFile(fmt.Sprintf("%s/split/%s/__all__.json", d.dir, o.GetNamespace()), p); err != nil {
-			errs = append(errs, err)
-		}
-		if err := d.writeToFile(fmt.Sprintf("%s/split/%s/%s.json", d.dir, o.GetNamespace(), o.GetKind()), p); err != nil {
-			errs = append(errs, err)
-		}
-	}
-	return multierr.Combine(errs...)
-}
-
-func (d *dirDumper) writeToFile(path string, b []byte) error {
-	f, err := d.file(path)
-	if err != nil {
-		return fmt.Errorf("failed to open file for copying: %w", err)
-	}
-	if _, err := f.Write(b); err != nil {
-		return fmt.Errorf("failed to copy to file: %w", err)
-	}
-	return nil
-}
-
-func (d *dirDumper) file(path string) (*os.File, error) {
-	f, ok := d.openFiles[path]
-	if ok {
-		return f, nil
-	}
-	dir := filepath.Dir(path)
-	if err := os.MkdirAll(dir, 0755); err != nil {
-		return nil, fmt.Errorf("failed to create directory %q: %w", dir, err)
-	}
-	f, err := os.Create(path)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create file %q: %w", path, err)
-	}
-	d.openFiles[path] = f
-	return f, nil
-}
-
-func groupVersionFromString(s string) schema.GroupVersion {
-	parts := strings.Split(s, "/")
-	if len(parts) == 1 {
-		return schema.GroupVersion{Version: parts[0]}
-	}
-	return schema.GroupVersion{Group: parts[0], Version: parts[1]}
 }
